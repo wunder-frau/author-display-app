271874c276372f91d9254a8a8c62df80c8c30bde
irene <wunder.frau@yandex.by>
2025-02-05 07:43:31 +0200

    styles for App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -49,26 +49,28 @@ const App: React.FC = () => {
   }
 
   return (
-    <div className='mx-auto min-h-[700px] w-full max-w-[1200px] min-w-[320px] bg-[#515174] text-center'>
-      <Router>
-        {/* <h2 className='animate-[var(--animate-shineImpulse)] text-center text-2xl text-blue-500'>
-          Hello, World!
-        </h2> */}
+    <div className='w-full bg-[#515174] py-4 sm:py-6 md:py-8 lg:py-12'>
+      <div className='w-full bg-[#515174] px-4 py-4 sm:px-6 sm:py-6 md:px-8 md:py-8 lg:px-12 lg:py-12 xl:px-16'>
+        <Router>
+          {/* <h2 className='animate-[var(--animate-shineImpulse)] text-center text-2xl text-blue-500'>
+            Hello, World!
+          </h2> */}
 
-        {error && <p className='error'>{error}</p>}
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <>
-                <Input addTitle={addTitle} />
-                <TitleList books={books} />
-              </>
-            }
-          />
-          <Route path='/book/:id' element={<BookPage books={books} />} />
-        </Routes>
-      </Router>
+          {error && <p className='error'>{error}</p>}
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <>
+                  <Input addTitle={addTitle} />
+                  <TitleList books={books} />
+                </>
+              }
+            />
+            <Route path='/book/:id' element={<BookPage books={books} />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   )
 }
