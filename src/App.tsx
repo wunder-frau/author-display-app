d91f8a3f79c6aef2616d56fd91ed870789ae9f75
irene <wunder.frau@yandex.by>
2025-02-16 00:34:34 +0200

    styles: update layout and styles using Headless UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useEffect } from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
+import BookListPage from './components/BookListPage'
+import BookPage from './components/BookPage'
 import { apiBaseUrl } from './constants'
 import { useResource } from './hooks'
 import { Book, NewBook } from './types'
-
-import BookListPage from './components/BookListPage'
-import BookPage from './components/BookPage'
 
 const App = () => {
   const [books, bookService] = useResource<Book, NewBook>(`${apiBaseUrl}/books`)
@@ -16,14 +15,12 @@ const App = () => {
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<BookListPage books={books} />} />
-          <Route path="/book/:id" element={<BookPage books={books} />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<BookListPage books={books} />} />
+        <Route path="/book/:id" element={<BookPage books={books} />} />
+      </Routes>
+    </Router>
   )
 }
 
